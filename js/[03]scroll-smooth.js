5d65d5620c32ee050438fabc48f008b325ec0444
banklerk <key4me@yandex.ru>
2020-07-08 16:59:00 +0300

    first commit

--- js/[03]scroll-smooth.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(() => {
-  var $page = $('html, body');
-  $('.header-list__link').click(function () {
-    $page.animate({
-      scrollTop: $($.attr(this, 'href')).offset().top
-    }, 1000);
-    return false;
-  });
-});
